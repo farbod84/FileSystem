97862831c812dc93cf2dde21704ca4ff9a81d2ec
Farbod <mob.misscall26@gmail.com>
2025-05-19 19:31:07 +0330

    Node.py debugged!

--- Node.py
+++ Node.py
@@ -15,6 +15,10 @@ class Node:
         node.parent = parent
 
     def move(self, parent):
+        if parent.is_ancestor(self):
+            print('DIRECTORY ERROR')
+            return
+        
         self.__parent.remove_child(self)
         parent.add_child(self)
         self.__parent = parent
@@ -110,6 +114,15 @@ class Directory(Node):
     def __sort_childs(self):
         self.__childs = sorted(self.__childs, key = lambda child: (0 if type(child) == Directory else 1, child.name))
 
+    def is_ancestor(self, value):
+        node = self
+        while node is not None:
+            if node is value:
+                return True
+            node = node.parent
+        
+        return False
+    
     def ls(self):
         self.__sort_childs()
         for child in self.__childs:
